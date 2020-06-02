abb55b559252bce871655de509fc46b404231e55
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-02 20:57:51 +0900

    당일 시간 선택제 #2 코드 변경 완성 코드 이거 없으면 실행 안됨

--- src/main/java/kiost/study/service/daySeatSelect.java
+++ src/main/java/kiost/study/service/daySeatSelect.java
@@ -11,9 +11,7 @@ import kiosk.study.dto.studyDTO;
 public class daySeatSelect implements KioskService{
 // #2 당일 시간제 결제 정보 저장
 	
-	// DAO 생성자 사용	
 	public studyDAO dao = new studyDAO();
-	
 	
 	@Override
 	public void execute(Model model) {
@@ -22,10 +20,6 @@ public class daySeatSelect implements KioskService{
 		// model 값 받아오기 위해서 두줄 사용
 
 		dao.daySeatSelect(dto);				// dto(좌석번호, 시간, 가격, 핸드폰)을 DB에 저장
-	}
-	
-	public void copyTable() {
-		dao.moveTable();
 	}
 
 }
