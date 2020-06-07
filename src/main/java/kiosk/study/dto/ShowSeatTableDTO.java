92a88658861f8dcb9f5a7182e4a895c25055c602
user <user@DESKTOP-RUGQLVK>
2020-06-07 18:50:56 +0900

    핸드폰 시간변경 Re

--- src/main/java/kiosk/study/dto/ShowSeatTableDTO.java
+++ src/main/java/kiosk/study/dto/ShowSeatTableDTO.java
@@ -3,14 +3,14 @@ package kiosk.study.dto;
 public class ShowSeatTableDTO {
 	//당일 좌석 사용 유무
 	private int seatNum;	//좌석 번호
-	private String phoneNum; //핸드폰 번호
+	private String toDate;  //오늘 날짜
 	private String endTime; //종료 시간
 	
 	public int getSeatNum() {return seatNum;}
 	public void setSeatNum(int seatNum) {this.seatNum = seatNum;}
 	
-	public String getPhoneNum() {return phoneNum;}
-	public void setPhoneNum(String phoneNum) {this.phoneNum = phoneNum;}
+	public String getToDate() {return toDate;}
+	public void setToDate(String toDate) {this.toDate = toDate;}
 	
 	public String getEndTime() {return endTime;}
 	public void setEndTime(String endTime) {this.endTime = endTime;}
