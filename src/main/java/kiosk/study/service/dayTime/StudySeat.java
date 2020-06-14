7db22a96e6a13a0b3b5d88fd64e83a2074c99316
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-15 08:15:13 +0900

    좌석은update문이므로 function호출시 매번 새로 생성자를 만들어줘야함

--- src/main/java/kiosk/study/service/dayTime/StudySeat.java
+++ src/main/java/kiosk/study/service/dayTime/StudySeat.java
@@ -10,19 +10,25 @@ import kiosk.study.dao.studySeatDAO;
 import kiosk.study.dto.studyDTO;
 
 public class StudySeat {
+	// 좌석은 sql문이 update되므로, pulblic으로 빼면 오류 발생하고 메소드를 부를때마다 새로 생성해 줘야 오류가 발생하지 않음.
 
-	public studySeatDAO dao = new studySeatDAO();
 	public Map<String, Object> map;
 	public studyDTO dto;
 	
 	public void seatEmptyCheck(Model model) {
 		map = model.asMap();
 		HttpServletRequest request = (HttpServletRequest)map.get("request");
+		studySeatDAO dao = new studySeatDAO();
 		
 		if(dao.seatEmptyCheck(request.getParameter("seatNum"))==0) {
 			model.addAttribute("result", 0);
 		}else {
 			model.addAttribute("result", 1);
 		} 
+	}
+	public void UpdateSeatInfo(Model model) {
+		// 당일 좌석 좌석 확인 구현하기
+		studySeatDAO dao = new studySeatDAO();
+		dao.updateSeatInfo();
 	}
 }
