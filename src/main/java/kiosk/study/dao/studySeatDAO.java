29cc02fb0caf5a76d148d1e29ca2a0a51df8636c
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-15 07:53:56 +0900

    sql문 이해했음 주석 삭제

--- src/main/java/kiosk/study/dao/studySeatDAO.java
+++ src/main/java/kiosk/study/dao/studySeatDAO.java
@@ -11,19 +11,15 @@ public class studySeatDAO {
 		this.template = Constant.template;
 	}
 
-	// 당일 좌석 카테고리 선택 시(배치도 보여줄때마다 작동)
-	//// sql문 수정. 기존 sql은 좌석번호 기준으로 partition되어 날짜가 다양하면 값을 가지고 오지 못했음. PARTITION BY
-	// seatNum => PARTITION BY todate
+	// 당일 좌석 카테고리 선택 시(배치도 보여줄때마다 작동) - 오류발생 가능성 : 날짜가 다양하면 값수령x
 	public void updateSeatInfo() {
-		// todaytotalSeat에 기록된 목록에서 오늘 날짜로 좌석의 가장 최근 시간값을 가져와서 showtodaystudyseat에
-		// update.
+		// 결제된 study_timeSet의 값을 todaytotalSeat에 복사해오고 그 값을 showtodaystudyseat에 업데이트
 		String updateSeatInfo = "update SHOWTODAYSTUDYSEAT set showtodaystudyseat.endtime =( "
 				+ "SELECT  TS.endtime FROM( "
 				+ "SELECT ROW_NUMBER() OVER(PARTITION BY todaytotalSeat.seatNum ORDER BY todaytotalSeat.ENDTIME DESC ) AS RNUM, todaytotalSeat.* "
 				+ "FROM todaytotalSeat ) TS WHERE RNUM = 1 and todate=(to_char(sysdate,'yyyy/mm/dd')) and seatNum = showtodaystudyseat.seatnum)";
 
-		// 만일 showtodaystudyseat의 endtime이 사용자가 페이지에 접속한 시간보다 전이라면 사용이 끝난 것이므로 null로
-		// 바꿔준다
+		// 만일 showtodaystudyseat의 endtime<현재시간 : 사용종료 null로 변경
 		String resetSeatInfo = "update SHOWTODAYSTUDYSEAT set endtime = null where endtime<to_char(sysdate,'hh24:mi:ss')";
 
 		template.update(updateSeatInfo);
@@ -31,20 +27,20 @@ public class studySeatDAO {
 	}
 
 	// (당일) 좌석에서 만일 사람이 있는 좌석을 선택했다면 결제창으로 넘어가지 못하게 하기 위한 sql문... -> 스터디룸과 예약좌석은 DB를
-	// 따로 둘거면 다른 메소드 생성 필요
-	public int seatEmptyCheck(String seatNum) {
-		try {
-			String sql = "select EndTIME from SHOWTODAYSTUDYSEAT where seatNum='" + seatNum + "'";
-			String result = template.queryForObject(sql, String.class); // null이면 비어있는 자리. 값이 있으면 사용자가 있는 자리
+		// 따로 둘거면 다른 메소드 생성 필요
+		public int seatEmptyCheck(String seatNum) {
+			try {
+				String sql = "select EndTIME from SHOWTODAYSTUDYSEAT where seatNum='" + seatNum + "'";
+				String result = template.queryForObject(sql, String.class); // null이면 비어있는 자리. 값이 있으면 사용자가 있는 자리
 
-			if (result.equals("null")) {
+				if (result.equals("null")) {
+					return 0;
+				} else {
+					return 1;
+				}
+			} catch (Exception e) {
 				return 0;
-			} else {
-				return 1;
 			}
-		} catch (Exception e) {
-			return 0;
 		}
-	}
 
 }
