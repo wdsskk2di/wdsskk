18360e9449f7089d67e3c91108793c9a1c3a3944
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-16 08:10:22 +0900

    [실행x] 스터디룸 좌석 배치 보여주는 function만 구현

--- src/main/java/kiosk/study/dao/studySeatDAO.java
+++ src/main/java/kiosk/study/dao/studySeatDAO.java
@@ -31,20 +31,38 @@ public class studySeatDAO {
 	}
 
 	// (당일) 좌석에서 만일 사람이 있는 좌석을 선택했다면 결제창으로 넘어가지 못하게 하기 위한 sql문... -> 스터디룸과 예약좌석은 DB를
-		// 따로 둘거면 다른 메소드 생성 필요
-		public int seatEmptyCheck(String seatNum) {
-			try {
-				String sql = "select EndTIME from SHOWTODAYSTUDYSEAT where seatNum='" + seatNum + "'";
-				String result = template.queryForObject(sql, String.class); // null이면 비어있는 자리. 값이 있으면 사용자가 있는 자리
+	// 따로 둘거면 다른 메소드 생성 필요
+	public int seatEmptyCheck(String seatNum) {
+		try {
+			String sql = "select EndTIME from SHOWTODAYSTUDYSEAT where seatNum='" + seatNum + "'";
+			String result = template.queryForObject(sql, String.class); // null이면 비어있는 자리. 값이 있으면 사용자가 있는 자리
 
-				if (result.equals("null")) {
-					return 0;
-				} else {
-					return 1;
-				}
-			} catch (Exception e) {
+			if (result.equals("null")) {
 				return 0;
+			} else {
+				return 1;
 			}
+		} catch (Exception e) {
+			return 0;
 		}
+	}
 
+		// 값 변경 아직 미구현 생성만 해둔 상태 스터디룸 좌석 확인
+	public void updateSeatInfo2() {
+		try {
+			// 결제된 study_timeSet의 값을 todaytotalSeat에 복사해오고 그 값을 showtodayRoomseat에 업데이트
+			String updateSeatInfo = "update showtodayRoomseat set showtodayRoomseat.endtime =( "
+					+ "SELECT  TS.endtime FROM( "
+					+ "SELECT ROW_NUMBER() OVER(PARTITION BY todaytotalSeat.seatNum ORDER BY todaytotalSeat.ENDTIME DESC ) AS RNUM, todaytotalSeat.* "
+					+ "FROM todaytotalSeat ) TS WHERE RNUM = 1 and todate=(to_char(sysdate,'yyyy/mm/dd')) and seatNum = showtodayRoomseat.seatnum)";
+
+			// 만일 showtodayRoomseat의 endtime<현재시간 : 사용종료 null로 변경
+			String resetSeatInfo = "update showtodayRoomseat set endtime = null where endtime<to_char(sysdate,'hh24:mi:ss')";
+
+			template.update(updateSeatInfo);
+			template.update(resetSeatInfo);
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
 }
