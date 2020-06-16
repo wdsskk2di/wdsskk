6de16ec31233a70a6001f4c735eb21ce5436193d
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-16 09:18:40 +0900

    당일 시간제랑 모델 스타일을 비슷하게 하기 위해 세부 코드 조정 HttpServletRequest 이용

--- src/main/java/kiost/study/service/ReserveStateService.java
+++ src/main/java/kiost/study/service/ReserveStateService.java
@@ -2,6 +2,8 @@ package kiost.study.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.ui.Model;
 
 import kiosk.study.dao.ReserveDAO;
@@ -9,11 +11,29 @@ import kiosk.study.dao.StudyRoomDAO;
 
 public class ReserveStateService {
 	
+	/*
+	public Map<String, Object> map;
+	
+	// #1 사용자 좌석 선택 : 통계치
+		public void daySeat_common(Model model) {
+			map = model.asMap();
+			HttpServletRequest request = (HttpServletRequest)map.get("request");
+			// model 값 받아오기 위해서 두줄 사용
+
+			int seatNum = Integer.parseInt(request.getParameter("seatNum"));
+	 
+			// 좌석 선택 값을 DB에 저장
+			dao.statSeat(seatNum);
+		}
+		*/
+	
 	// 예약 당일 좌석값
 	public void reserveToday(Model model) {
 		Map<String, Object> map = model.asMap();
-		String seatNum = (String) map.get("seatNum");
-		String title = (String) map.get("title");
+		HttpServletRequest request = (HttpServletRequest)map.get("request");
+		
+		String seatNum = request.getParameter("seatNum");
+		String title = request.getParameter("title");
 		
 		ReserveDAO daoRs = new ReserveDAO();
 		StudyRoomDAO daoRo = new StudyRoomDAO();
