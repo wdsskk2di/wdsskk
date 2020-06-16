1396df191ad292b4cbb113dd85aaede6fd3d1385
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-16 09:18:52 +0900

    변경된service를 controller에 적용

--- src/main/java/kiosk/study/home/PaymentController.java
+++ src/main/java/kiosk/study/home/PaymentController.java
@@ -72,23 +72,17 @@ public class PaymentController {
 				return "payment";	//결제 페이지로
 				
 			}else if(title.equals("s") && num > 40 && num < 44){ // 스터디룸 + 입력값이 41~43 사이				
-				//스터디룸의 타임테이블				
-				rs.reserveToday(model);		
-				
-				// 스터디룸과 예약제 시간 나눔 변경 수정중 
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+				//스터디룸의 타임테이블	
 				
 				model.addAttribute("seatNum", num);
+				
+				
+				// 스터디룸과 예약제 시간 나눔 변경 수정중  *****************************************************
+				rs.reserveToday(model);		
+
+				
+				
+
 				return "payment";	//결제 페이지로
 				
 			}else {//입력된 좌석에 문제가 있는 경우
@@ -102,16 +96,7 @@ public class PaymentController {
 
 	}
 
-	/*
-	 else if(title.equals("r") && num > 20 && num < 41){  //예약 좌석 + 입력값이21~40 사이				
-		//이미 누군가 있다면 입력되지 않게 돌려야..
-		ss.seatEmptyCheck(model);	
-				
-		model.addAttribute("seatNum", num);
-		return "payment";	//결제 페이지로
-				
-	}
-	 */
+
 	
 	//당일 좌석, 당일 스터디룸 결제
 	@PostMapping("paymentCheck")
